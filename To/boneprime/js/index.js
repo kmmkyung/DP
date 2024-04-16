5e5883eccacede0d050a30d5d9f1855ce358dd0d
Min Kyung <01aldrn10@gmail.com>
2024-04-16 23:01:58 +0900

    s

--- To/boneprime/js/index.js
+++ To/boneprime/js/index.js
@@ -1,30 +1,19 @@
 window.addEventListener('DOMContentLoaded',function(){
 
   // 메인 메뉴 - 서브 메뉴 보이기
-  function menuShow(){
     const navItem = document.querySelectorAll('.nav-item');
     const subNavList = document.querySelectorAll('.subNav-list')
+    const subNavListWrap = document.querySelectorAll('.subNav-list__wrap')
+
     navItem.forEach(function(ele,idx){
       let subNavListHeight = subNavList[idx].clientHeight;
+      
       ele.addEventListener('mouseenter',function(){
-        // subNavList[idx].classList.add('active')
-        subNavList[idx].style.height = subNavListHeight+'px'
+        console.log(subNavListHeight);
+        subNavListWrap[idx].style.height = subNavListHeight+'px'
       })
       ele.addEventListener('mouseleave',function(){
-        subNavList[idx].style.height = 0+'px'
+        subNavListWrap[idx].style.height = 0+'px'
       })
     })
-  }
-  menuShow() // 최초 호출
-
-  // 하단 상단으로 버튼 클릭시 페이지 상단 이동
-  function moveTop(){
-    const btn = document.querySelector('.footer-top');
-    const section1 = document.querySelector('.section1')
-    btn.addEventListener('click',function(){
-      window.scrollTo(0,0)
-      // section1.scrollIntoView({behavior:'smooth',black:'start'})
-    })
-  }
-  moveTop() // 최초 호출
 })
--- To/boneprime/js/sub.js
+++ To/boneprime/js/sub.js
@@ -1,4 +1,18 @@
 window.addEventListener('DOMContentLoaded',function(){
   const banner1 = document.querySelector('.section1 .banner .banner1')
   const banner2 = document.querySelector('.section1 .banner .banner2')
+  // setInterval(function(){
+  //   banner2.classList.toggle('fade')
+  // },5000)
+
+  let opa = 0
+  setInterval(function(){
+    if( opa < 1 ){
+      opa++;
+    }
+    else{
+      opa--;
+    }
+    banner2.style.opacity = opa
+  },5000)
 })
