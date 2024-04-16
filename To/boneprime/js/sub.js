cc1cdfcba23fe4c6fa6ccf817d1b9154a3784c1a
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-04-16 18:23:08 +0900

    s

---
+++ To/boneprime/js/sub.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded',function(){
+  const banner1 = document.querySelector('.section1 .banner .banner1')
+  const banner2 = document.querySelector('.section1 .banner .banner2')
+})
