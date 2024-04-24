c957603c91d86f5528350d7808f3a2d45f106452
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-04-24 14:48:27 +0900

    s

--- 03. 아몬드성형외과/js/common.js
+++ 03. 아몬드성형외과/js/common.js
@@ -14,13 +14,28 @@ window.addEventListener('DOMContentLoaded',function(){
 
   // 메뉴버튼 누르면 서브메뉴 보여짐
   const menuList = document.querySelectorAll('.menuAll-menu .menu-list .list-item h6')
+  const menuListLine = document.querySelectorAll('.menuAll-menu .menu-list .list-item .menu-line')
   const subMenuListWrap = document.querySelectorAll('.menuAll-menu .subMenu-wrap')
   const subMenuList = document.querySelectorAll('.menuAll-menu .subMenu-list')
+  const textList = document.querySelectorAll('.menuAll-menu .text-list .list-item')
   menuList.forEach(function(ele,idx){
     let subMenuHeight =  subMenuList[idx].clientHeight;
     ele.addEventListener('click',function(){
-      
+      const menuListActive = document.querySelector('.menuAll-menu .menu-list .list-item h6.active')
+      const subMenuListWrapActive = document.querySelector('.menuAll-menu .subMenu-wrap.active')
+      const textListActive = document.querySelector('.menuAll-menu .text-list .list-item.active')
+      if(menuListActive){
+        menuListActive.classList.remove('active')
+        subMenuListWrapActive.classList.remove('active')
+        subMenuListWrapActive.style.height = 0 + 'px'
+        textListActive.classList.remove('active')
+      }
 
+      subMenuListWrap[idx].style.height = subMenuHeight + 'px'
+      menuList[idx].classList.add('active')
+      subMenuListWrap[idx].classList.add('active')
+      textList[idx].classList.add('active')
     })
   })
 })
+
