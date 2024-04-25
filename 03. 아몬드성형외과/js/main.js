006967c22cc7fe026cc982ca9c5b1724274ed736
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-04-25 18:00:14 +0900

    s

--- 03. 아몬드성형외과/js/main.js
+++ 03. 아몬드성형외과/js/main.js
@@ -3,4 +3,23 @@ window.addEventListener('DOMContentLoaded',function(){
   function section2Video(){
     const video = document.querySelectorAll('.section2 .video')
   }
+
+  // window.addEventListener('resize',function(){
+  //   const section5Swiper = document.querySelector('.section5 .swiper')
+  //   if(window.innerWidth >900){
+  //     section5Swiper.destroy(true, true)
+  //   }
+  //   else{
+  //     const swiper = new Swiper('.swiper', {
+  //       // If we need pagination // 페이지 순서 원형 이미지
+  //       pagination: {
+  //         el: '.swiper-pagination', // 요소 정의
+  //         clickable: true // 클릭 가능 여부
+  //       },
+  //     });
+  //   }
+    
+  // })
+
+  
 })
