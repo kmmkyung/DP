9e1411351a8bfb32309db4b0c4356c8c902dabe9
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-04-26 13:38:14 +0900

    s

--- 03. 아몬드성형외과/js/main.js
+++ 03. 아몬드성형외과/js/main.js
@@ -1,17 +1,21 @@
 window.addEventListener('DOMContentLoaded',function(){
 
-
   // section1
-  const section1 = document.querySelector('.section1')
+  const section1 = document.querySelector('main .section1')
   let section1Typed = new Typed('.section1-title',{
     strings:['눈이 예쁘다, 그래서 아몬드'],
     typeSpeed: 200,
   })
-  
+
   // section2
     const section2 = document.querySelector('.section2')
     const video = document.querySelector('.section2 .video')
+    video.pause()
     
+
+  // section3
+  const section3 = document.querySelector('.section3')
+
   // window.addEventListener('resize',function(){
   //   const section5Swiper = document.querySelector('.section5 .swiper')
   //   if(window.innerWidth >900){
@@ -30,43 +34,38 @@ window.addEventListener('DOMContentLoaded',function(){
   // })
 
     // all
+    let ioOptions = {
+      // root:document.querySelector('main'),
+      rootMargin:'30%',
+      threshold: [1.0],
+    }
     let io = new IntersectionObserver(function(entries){
-      entries.forEach( ele => {
-        if(ele.target == section1){
-          if(entries[0].isIntersecting){
-            console.log('관찰1');
-            section1Typed.reset()          
+      entries.forEach((entry,idx)=>{
+          if(entry.target == section1){
+            console.log('시작1');
+            section1Typed.reset()      
           }
-          else{
-            console.log('종료1');
-          }
-        }
-        if(ele.target == section2){
-          if(entries[1].isIntersecting){
-            console.log('관찰2');
+          if(entry.target == section2){
+            console.log('시작2');
             video.play()
           }
-          else{
-            console.log('종료2');
-            video.pause()
+          if(entry.target == section3){
+            console.log('시작3');
           }
-        }
+      },ioOptions)
+      
+        // if(entries[1].isIntersecting){
+        //   if(!entries[1].isIntersecting){
+        //     console.log('종료2');
+        //     video.pause()
+        //   }
+        //   else{
+        //     console.log('관찰2');
+        //     video.play()
+        //   }
+        // }
+    })
 
-          // if(entries[1].isIntersecting){
-            //   console.log('section2');
-          //     // video.forEach(function(ele){
-            //     //   ele.play()
-            //     // })
-            //   }
-            // if(entries[2].isIntersecting){
-              //   console.log('section3');
-              //   }
-      })
-    })
-    const entry = document.querySelectorAll('.entry')
-    entry.forEach(function(ele){
-      io.observe(ele)
-    })
-  
-  
+  const entry = document.querySelectorAll('.entry')
+  entry.forEach (ele=> io.observe(ele))  
 })
