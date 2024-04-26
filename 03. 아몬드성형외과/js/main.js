2f27fb9a23b1527c67bf2d462b946e5043d44f3e
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-04-26 11:01:29 +0900

    s

--- 03. 아몬드성형외과/js/main.js
+++ 03. 아몬드성형외과/js/main.js
@@ -1,9 +1,17 @@
 window.addEventListener('DOMContentLoaded',function(){
-  // 비디오가 화면에 들어왔으때 재생 나가면 멈춤
-  function section2Video(){
-    const video = document.querySelectorAll('.section2 .video')
-  }
 
+
+  // section1
+  const section1 = document.querySelector('.section1')
+  let section1Typed = new Typed('.section1-title',{
+    strings:['눈이 예쁘다, 그래서 아몬드'],
+    typeSpeed: 200,
+  })
+  
+  // section2
+    const section2 = document.querySelector('.section2')
+    const video = document.querySelector('.section2 .video')
+    
   // window.addEventListener('resize',function(){
   //   const section5Swiper = document.querySelector('.section5 .swiper')
   //   if(window.innerWidth >900){
@@ -21,5 +29,44 @@ window.addEventListener('DOMContentLoaded',function(){
     
   // })
 
+    // all
+    let io = new IntersectionObserver(function(entries){
+      entries.forEach( ele => {
+        if(ele.target == section1){
+          if(entries[0].isIntersecting){
+            console.log('관찰1');
+            section1Typed.reset()          
+          }
+          else{
+            console.log('종료1');
+          }
+        }
+        if(ele.target == section2){
+          if(entries[1].isIntersecting){
+            console.log('관찰2');
+            video.play()
+          }
+          else{
+            console.log('종료2');
+            video.pause()
+          }
+        }
+
+          // if(entries[1].isIntersecting){
+            //   console.log('section2');
+          //     // video.forEach(function(ele){
+            //     //   ele.play()
+            //     // })
+            //   }
+            // if(entries[2].isIntersecting){
+              //   console.log('section3');
+              //   }
+      })
+    })
+    const entry = document.querySelectorAll('.entry')
+    entry.forEach(function(ele){
+      io.observe(ele)
+    })
+  
   
 })
