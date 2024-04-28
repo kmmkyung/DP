a1e20f9f97ae7d273e270d31d00faf6ecf6e81ab
Min Kyung <01aldrn10@gmail.com>
2024-04-28 18:53:43 +0900

    s

--- 03. 아몬드성형외과/js/main.js
+++ 03. 아몬드성형외과/js/main.js
@@ -1,55 +1,73 @@
 window.addEventListener('DOMContentLoaded',function(){
 
-     // all
-    $(document).ready(function() {
-      $('#fullpage').fullpage({
-        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE', 
-        showActiveTooltip: false,
-        navigation: true,  
-        navigationPosition: 'left',
-        navigationTooltips: ['아몬드', '병원전경','의료진소개','특화수술','리얼모델후기','리얼셀피','약도'],
-        scrollHorizontally: true,
-        autoScrolling:true,
-        fitToSection: true,
-        fitToSectionDelay: 600,
-        observer: true,
-        scrollingSpeed: 1500,
-        keyboardScrolling: true,
-        onLeave: function(anchorLink,index){
-          console.log(index);
+  const logoPcB = $('.logo-pc__b')
+  const logoPcW = $('.logo-pc__w')
+  const logoMobileB = $('.logo-mobile__b')
+  const logoMobileW = $('.logo-mobile__w')
 
-          if(index == 2){
-            if(window.innerWidth > 768){
-              videoPc.play()
-            }
-            if(window.innerWidth <= 768){
-              videoMobile.play()
-            }
+
+    // all
+  $(document).ready(function() {
+    $('#fullpage').fullpage({
+      licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE', 
+      showActiveTooltip: false,
+      navigation: true,  
+      navigationPosition: 'left',
+      navigationTooltips: ['아몬드', '병원전경','의료진소개','특화수술','리얼모델후기','리얼셀피','약도'],
+      scrollHorizontally: true,
+      autoScrolling:true,
+      fitToSection: true,
+      fitToSectionDelay: 600,
+      observer: true,
+      scrollingSpeed: 1500,
+      keyboardScrolling: true,
+      onLeave: function(anchorLink,index){
+        if(index == 2){
+          if(window.innerWidth > 768){
+            videoPc.play()
           }
-          if(index == 6){
-            section6Page1.classList.add('active')
-          }
-          else if(index != 6){
-            section6Page1.classList.remove('active')
+          if(window.innerWidth <= 768){
+            videoMobile.play()
           }
         }
-      });
+        if(index == 5){
+          const navigationText = $('#fp-nav .fp-tooltip')          
+          logoPcW.removeClass('-hidden')
+          logoPcB.addClass('-hidden')
+          logoMobileW.removeClass('-hidden')
+          logoMobileB.addClass('-hidden')
+          navigationText.css({"color":"#fff"})
+        }
+        if(index != 5){
+          logoPcB.removeClass('-hidden')
+          logoPcW.addClass('-hidden')
+          logoMobileB.removeClass('-hidden')
+          logoMobileW.addClass('-hidden')
+        }
+        if(index == 6){
+          section6Page1.classList.add('active')
+        }
+        else if(index != 6){
+          section6Page1.classList.remove('active')
+        }
+      }
     });
+  });
 
-    let ioOption = {
-      threshold: 1
-    }
-    let io = new IntersectionObserver(function(entries){
-      entries.forEach((entry)=>{
-        if(entry.isIntersecting){
-          section1Typed.reset()
-          entry.target.classList.add('active')
-        }
-        else{
-          entry.target.classList.remove('active')
-        }
-      },ioOption)
-    })
+  let ioOption = {
+    threshold: 1
+  }
+  let io = new IntersectionObserver(function(entries){
+    entries.forEach((entry)=>{
+      if(entry.isIntersecting){
+        section1Typed.reset()
+        entry.target.classList.add('active')
+      }
+      else{
+        entry.target.classList.remove('active')
+      }
+    },ioOption)
+  })
 
   const entry = document.querySelectorAll('.entry')
   entry.forEach (ele => io.observe(ele))  
@@ -96,7 +114,10 @@ window.addEventListener('DOMContentLoaded',function(){
     
     if(window.innerWidth <= 768 && swipe5rVal == undefined){
       swipe5rVal = new Swiper('.section5-swiper', {
-      slidesPerView: 2.5,
+      slidesPerView: 2,
+      loop:true,
+      spaceBetween: 10, 
+      centeredSlides: true,
       simulateTouch: true,
         pagination: {
           el: '.swiper-pagination',
