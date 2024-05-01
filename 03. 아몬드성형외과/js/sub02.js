f7838fdb8be5e2df1523b0cf634e2d60073adbc5
Min Kyung <01aldrn10@gmail.com>
2024-05-01 23:33:15 +0900

    s

---
+++ 03. 아몬드성형외과/js/sub02.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded',function(){
+  // twenty
+  $(function(){
+    $(".item-twenty").twentytwenty({
+      default_offset_pct: 0.7, // How much of the before image is visible when the page loads
+      no_overlay: true, //Do not show the overlay with before and after
+      move_slider_on_hover: true, // Move slider on mouse hover?
+      move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
+      click_to_move: false // 
+    });
+  });
+})
