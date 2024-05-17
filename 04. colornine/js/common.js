059a6c430e779ed00c6d1d73f7942355ae984eb2
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-05-17 17:54:53 +0900

    s

---
+++ 04. colornine/js/common.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded',function(){
+  // header submenu 보이게하기
+  const menuTitle = document.querySelectorAll('.mainMenu-list .menuTitle')
+  const subMenuWrap = document.querySelectorAll('.mainMenu-list .subMenu-wrap')
+  const subMenu = document.querySelectorAll('.mainMenu-list .subMenu-list')
+
+
+  menuTitle.forEach(function(ele,idx){
+    let vh = subMenu[idx].clientHeight;
+
+    ele.addEventListener('mouseenter',function(){
+      subMenuWrap[idx].style.height = vh + "px";
+    })
+    ele.addEventListener('mouseleave',function(){
+      subMenuWrap[idx].style.height = 0;
+    })
+  })
+
+  // header 버튼 누르면 전체메뉴 보이게 하기
+  const menuButton = document.querySelector('.menu-button')
+  const menuAll = document.querySelector('.menu-all')
+  menuButton.addEventListener('click',function(){
+    menuAll.classList.add('active')
+  })
+})
