b775f4085a4116aa37eff46f16f648ede58cce01
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-06-26 11:19:14 +0900

    s

--- 05.data/js/common.js
+++ 05.data/js/common.js
@@ -1,9 +1,23 @@
 window.addEventListener('DOMContentLoaded',function(){
+  // 타블렛 모드 메뉴 보이기
   const menuBtn = document.querySelector('.menu-more')
-  const menuWrap = document.querySelector('.header-wrap')
+  const menu = document.querySelector('header')
   const menuBg = document.querySelector('.menu-bg')
   menuBtn.addEventListener('click',function(){
-    menuWrap.classList.toggle('active')
+    menu.classList.toggle('active')
     menuBg.classList.toggle('active')
+  })
+
+  // dark / light mode
+  const body = document.querySelector('body')
+  const darkBtn = document.querySelector('.mode-wrap img')
+  darkBtn.addEventListener('click',function(){
+    body.classList.toggle('dark')
+    if(body.classList.contains('dark')){
+      darkBtn.setAttribute('src','../images/light-mode.png')
+    }
+    else{
+      darkBtn.setAttribute('src','../images/dark-mode.png')
+    }
   })
 })
