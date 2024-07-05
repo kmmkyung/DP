d2c1dfb03647f27229b1f0c471d89209ded15b74
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-07-05 17:20:38 +0900

    s

---
+++ 05.data/js/admin-04.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded',function(){
+  const tapBtn = document.querySelectorAll('.right-4_content1 .tap')
+  const tapCon = document.querySelectorAll('.right-4_content1 .tapCon')
+  console.log(tapCon);
+  
+  tapBtn.forEach(function(ele,idx){
+    ele.addEventListener('click',function(){
+    })
+  })
+})
