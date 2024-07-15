0897b52df43f9ac357b118c017f9a15e312a2460
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-07-15 17:23:00 +0900

    s

--- 07.CQvista/js/ani.js
+++ 07.CQvista/js/ani.js
@@ -1,5 +1,17 @@
-window.addEventListener('DOMContentLoaded',function{
+window.addEventListener('DOMContentLoaded',function(){
 
     const triangle = document.querySelectorAll('.triangle')
-    
+    const keyframes = [
+			{ opacity: 0 },
+			{ opacity: 1 }
+		];
+
+		const options = {
+			duration: 1500,
+			iterations: Infinity,
+		};
+
+    triangle.forEach(function(ele,idx){
+      ele.animate(keyframes,{...options,delay:idx*100})
+    })
 })
