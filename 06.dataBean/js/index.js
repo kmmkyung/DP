faf9714c871e0403d1ba4eb00967740215344fb6
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-07-18 15:38:42 +0900

    s

--- 06.dataBean/js/index.js
+++ 06.dataBean/js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded',function(){
   // 헤더 숨기기 / 배경색상변경
-  const header = document.querySelector('header')
+  const header = document.querySelector('.pc-header')
   window.addEventListener('wheel',function(event){
     if(event.wheelDeltaY < 0){
       header.classList.remove('active')
@@ -16,10 +16,18 @@ window.addEventListener('DOMContentLoaded',function(){
       header.style.backgroundColor = 'transparent'
     }
     else{
-      header.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+      header.style.backgroundColor = 'rgba(0, 0, 0, 0.8 )'
     }
   })
 
-  // 메뉴 클릭시 이동
-    
+  // 모바일 메뉴
+  const mobileHam = document.querySelector('.ham');
+  const mobileMenu = document.querySelector('.mobileMenu-wrap') 
+  const mobileMenuBg = document.querySelector('.header-bg') 
+  mobileHam.addEventListener('click',function(){
+    mobileHam.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
+    mobileMenuBg.classList.toggle('active')
+  })
+  
 })
