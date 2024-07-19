edb6b45a8e042ea31172a6e83e793c229e96d763
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-07-19 18:04:27 +0900

    s

--- 06.dataBean/js/index.js
+++ 06.dataBean/js/index.js
@@ -1,4 +1,7 @@
 window.addEventListener('DOMContentLoaded',function(){
+  //AOS
+  AOS.init(); 
+
   // 헤더 숨기기 / 배경색상변경
   const header = document.querySelector('.pc-header')
   window.addEventListener('wheel',function(event){
@@ -22,14 +25,37 @@ window.addEventListener('DOMContentLoaded',function(){
 
   // 모바일 메뉴
   const mobileHam = document.querySelector('.ham');
-  const mobileMenu = document.querySelector('.mobileMenu-wrap') 
+  const mobileMenuWrap = document.querySelector('.mobileMenu-wrap') 
   const mobileMenuBg = document.querySelector('.header-bg') 
   mobileHam.addEventListener('click',function(){
     mobileHam.classList.toggle('active')
-    mobileMenu.classList.toggle('active')
+    mobileMenuWrap.classList.toggle('active')
     mobileMenuBg.classList.toggle('active')
   })
+
+  // 메뉴 클릭시 이동
+  const pcMenu = document.querySelectorAll('.pcMenu');
+  const mobileMenu = document.querySelectorAll('.mobileMenu');
+  const contentPage = document.querySelectorAll('.contentPage');
   
+  function movePage(ele,idx){
+    ele.addEventListener('click',function(event){
+      event.preventDefault()
+      let pageVh = contentPage[idx].offsetTop;
+      window.scroll({top: pageVh, behavior: "smooth"})
+    })
+  }
+
+  pcMenu.forEach(function(ele,idx){
+    movePage(ele,idx)
+  })
+  mobileMenu.forEach(function(ele,idx){
+    movePage(ele,idx)
+  })
+
+
+
+
   // section6-swiper
   section6Swiper = new Swiper('.section6-swiper', {
     spaceBetween: 40,
@@ -49,4 +75,39 @@ window.addEventListener('DOMContentLoaded',function(){
       el: '.swiper-pagination',
     },
   });
+
+  // section7-swiper
+  section6Swiper = new Swiper('.section7-swiper', {
+    spaceBetween: 40,
+    slidesPerView: 1,
+    loop:true,
+    // autoplay: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      425: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+  // company
+  const companyMenu = document.querySelectorAll('.section7 .sub-menu li')
+  const companyContent = document.querySelectorAll('.companyContent')
+  companyMenu.forEach(function(ele,idx){
+    ele.addEventListener('click',function(){
+      for(let i=0; i<companyContent.length; i++){
+        companyMenu[i].classList.remove('active')
+        companyContent[i].classList.add('displayNone')
+      }
+      ele.classList.add('active')
+      companyContent[idx].classList.remove('displayNone')
+    })
+  })
 })
+
