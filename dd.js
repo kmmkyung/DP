a50750047e1be0264d011c8f509ff1a45978d8c7
kmmkyung <119033846+kmmkyung@users.noreply.github.com>
2026-03-19 19:49:18 +0900

    s

--- dd.js
+++ dd.js
@@ -3,4 +3,5 @@
 window.addEventListener("DOMContentLoaded", function () {
   const floatBtn = document.querySelector(".float .toggleBtn ");
   const floatList = document.querySelector(".float ul ");
+  floatBtn.addEventListener("click", function () {});
 });
