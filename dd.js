815d0b117b4f6a4bdb529bd810764d7294765df3
kmmkyung <119033846+kmmkyung@users.noreply.github.com>
2026-04-08 18:35:15 +0900

    정리

--- dd.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-window.addEventListener("DOMContentLoaded", function () {
-  const floatBtn = document.querySelector(".float .toggleBtn ");
-  const floatList = document.querySelector(".float ul ");
-  floatBtn.addEventListener("click", function () {});
-});
